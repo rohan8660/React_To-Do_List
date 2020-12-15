ab552b161ddd828869dfd2f227dcf6fe60b8ecaa
RohN <40888098+rohan8660@users.noreply.github.com>
2020-12-16 03:02:29 +0530

    Updates

---
+++ precache-manifest.acae86da98b59c9c8b116421a3678e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e863c74cca5e2af25e6702445a7e94c",
+    "url": "/React_To-Do_List/index.html"
+  },
+  {
+    "revision": "38eb4de3d582fec805fb",
+    "url": "/React_To-Do_List/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/React_To-Do_List/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_To-Do_List/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38eb4de3d582fec805fb",
+    "url": "/React_To-Do_List/static/js/main.1e9dd606.chunk.js"
+  },
+  {
+    "revision": "863e7e81df31de37ff59",
+    "url": "/React_To-Do_List/static/js/runtime-main.4422d691.js"
+  }
+]);
